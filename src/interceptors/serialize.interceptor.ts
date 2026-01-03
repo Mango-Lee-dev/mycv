c41f366fd80c1fd4164534abc147cae5920960d1
wootaiklee <fined000@naver.com>
2026-01-03 22:06:41 +0900

    Refactor SerializeInterceptor to utilize UserDto for response serialization; add UserDto class to define user data structure.

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,17 +1,15 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
 import { map, Observable } from 'rxjs';
+import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-    //  Run something before a request is handled by the request handler
-    console.log(
-      'I am running before the request is handled by the request handler',
-      context,
-    );
     return handler.handle().pipe(
-      map((data) => {
-        // Run something before the response is sent out
-        console.log('I am running before the response is sent out', data);
+      map((data: any) => {
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true,
+        });
       }),
     );
   }
