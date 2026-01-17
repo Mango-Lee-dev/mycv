acaba46da079b0b67c1265069942fa6ce645a1a2
wootaiklee <fined000@naver.com>
2026-01-17 21:13:53 +0900

    Update test:watch script to limit max workers and add VSCode settings for Prettier formatting; refactor auth service test to use Partial type for UsersService mock.

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -5,14 +5,14 @@ import { User } from './user.entity';
 
 it('can create an instance of auth service', async () => {
   // create a fake copy of the users service
-  const fakeUserService = {
+  const fakeUserService: Partial<UsersService> = {
     find: () => Promise.resolve([]),
-    create: (email: string, password: string) => {
-      return Promise.resolve({ id: 1, email, password } as User);
-    },
+    create: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password } as User),
   };
   const module = await Test.createTestingModule({
-    providers: [AuthService,
+    providers: [
+      AuthService,
       { provide: UsersService, useValue: fakeUserService },
     ],
   }).compile();
